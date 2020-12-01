0298537a46b4845c7e25b443a1c68ca56f89633e
Demiu <miazgapow@gmail.com>
2020-12-01 18:05:27 +0100

    Day 1 part 1 solved

---
+++ day1/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+     if let Ok(lines) = read_lines("./input.txt") {
+        let mut numbers = vec![];
+
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(line_str) = line {
+                if let Ok(number) = line_str.parse::<u32>() {
+                    numbers.push(number);
+                }
+            }
+        }
+
+        'search: for i in 0 .. numbers.len() {
+            for j in i .. numbers.len() {
+                let sum = numbers[i] + numbers[j];
+                if sum == 2020 {
+                    let mul = numbers[i] * numbers[j];
+                    println!("{}", mul);
+                    break 'search;
+                }
+            }
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
