87542219a8b0ea0c9699780e676a45979e3a7a4f
Demiu <miazgapow@gmail.com>
2020-12-02 11:51:31 +0100

    Day 2 Part 1 solved

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,7 +3,27 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("./input.txt") {
+        let mut valid_count: u32 = 0;
+
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(line_str) = line {
+                let parts: Vec<&str> = line_str.split(' ').collect();
+                let bounds: Vec<&str> = parts[0].split('-').collect();
+                let lower_bound = bounds[0].parse::<u32>().unwrap();
+                let upper_bound = bounds[1].parse::<u32>().unwrap();
+                let character: char = parts[1].chars().next().unwrap();
+                let password = parts[2];
+                let char_count = password.matches(character).count() as u32;
+                if char_count >= lower_bound && char_count <= upper_bound {
+                    valid_count += 1;
+                }
+            }
+        }
+
+        println!("Found {} valid passwords.", valid_count);
+    }
 }
 
 // The output is wrapped in a Result to allow matching on errors
