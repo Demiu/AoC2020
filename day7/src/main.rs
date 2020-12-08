5c24a37942ce56893c7068d2fc099c67c70c5460
Demiu <miazgapow@gmail.com>
2020-12-08 16:35:06 +0100

    Day 7 Part 2 solved

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
             "no" => vec![],
             num => {
                 let mut ret = vec![];
-                
+
                 let count: u32 = num.parse().unwrap();
                 let child_first_word = words.next().unwrap();
                 let child_second_word = words.next().unwrap();
@@ -41,14 +41,17 @@ fn main() {
     }
 
     let my_bag = "shiny gold".to_owned();
+
     let mut found = 0;
     for parent in rules.keys() {
         if bag_can_be_in(&my_bag, parent, &rules) {
             found += 1;
         }
     }
-
     println!("Part 1 answer: {}", found);
+
+    let bags_in_mine = total_bags_in_bag(&my_bag, &rules) - 1;
+    println!("Part 2 answer: {}", bags_in_mine);
 }
 
 fn bag_can_be_in(inner: &String, outer: &String, rules: &HashMap<String, Vec<(String, u32)>>) -> bool{
@@ -64,6 +67,15 @@ fn bag_can_be_in(inner: &String, outer: &String, rules: &HashMap<String, Vec<(St
     }
 
     return false;
+}
+
+// includes the "outer" bag
+fn total_bags_in_bag(bag: &String, rules: &HashMap<String, Vec<(String, u32)>>) -> u32 {
+    let mut total = 1;
+    for (subbag, count) in &rules[bag] {
+        total += count * total_bags_in_bag(subbag, &rules);
+    }
+    return total;
 }
 
 // The output is wrapped in a Result to allow matching on errors
