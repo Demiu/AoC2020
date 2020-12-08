d6725bd846d8875ea2345b4e1f760bf04b3ec727
Demiu <miazgapow@gmail.com>
2020-12-08 14:09:15 +0100

    Day 6 Part 2 solved

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -8,22 +8,41 @@ fn main() {
     file.read_to_string(&mut file_str).expect("Couldn't read input to string");
     let groups: Vec<_> = file_str.split("\n\n").collect();
 
-    let answer = groups.iter().fold(0, 
+    let answer1 = groups.iter().fold(0, 
         |sum, s| sum + s.chars().fold(HashMap::new(), 
             |mut map, c| if c != '\n' 
                 {
                     map.insert(c, 1);
-                    printMap(&map);
                     map
                 } else {map}
             ).keys().len());
     
-    println!("Part 1 answer: {}", answer);
-}
+    println!("Part 1 answer: {}", answer1);
 
-fn printMap(map: &HashMap<char, i32>) {
-    for (k, v) in map {
-        println!("Key: {} Value: {}", k, v);
-    }
-    println!("\n");
+    let answer2 = groups.iter().fold(0, 
+        |sum, s| sum + 
+            {
+                let mut chars_hashmap = s.chars().fold(HashMap::new(), 
+                    |mut map, c| 
+                        {
+                            match map.insert(c, 1) {
+                                Some(x) => map.insert(c, x+1),
+                                None    => None
+                            };
+                            map
+                        }
+                    );
+                // sadly, last line in input has a trailing newline that isn't split off
+                // so it has to be accounted for here
+                if s.chars().last().unwrap() == '\n' {
+                    *chars_hashmap.get_mut(&'\n').unwrap() -= 1;
+                }
+                let people_in_group = match chars_hashmap.get(&'\n') {
+                    Some(x) => x+1,
+                    None    => 1,
+                };
+                chars_hashmap.iter().fold(0, |sum, kvp| sum + if (*kvp.1) == people_in_group {1} else {0})
+            });
+    
+    println!("Part 2 answer: {}", answer2);
 }
