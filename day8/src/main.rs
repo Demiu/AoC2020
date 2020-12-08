47e456c75da549e34fa88c4c8acdb08fcfc7bd2d
Demiu <miazgapow@gmail.com>
2020-12-08 18:48:47 +0100

    Day 8 Part 2 solved

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -5,14 +5,47 @@ use std::path::Path;
 
 fn main() {
     let lines = read_lines("./input.txt").expect("Couldn't read input.txt");
-    let lines_vec: Vec<_> = lines.collect::<Result<_, _>>().expect("Couldn't collect lines into a vec");
+    let mut lines_vec: Vec<_> = lines.collect::<Result<_, _>>().expect("Couldn't collect lines into a vec");
 
+    let (_, acc) = run_till_infinite_or_end(&lines_vec);
+
+    println!("acc on first repeated instruction is: {}", acc);
+
+    let mut modified_acc = 0;
+    let instruction_count = lines_vec.len();
+    for i in 0..lines_vec.len() {
+        let new_line;
+        match &lines_vec[i][..3] {
+            "nop" => {
+                new_line = lines_vec[i].replace("nop","jmp");
+            },
+            "jmp" => {
+                new_line = lines_vec[i].replace("jmp","nop");
+            },
+            "acc" => continue,
+            u => panic!("Encountered unknown instruction code: {}", u)
+        }
+        
+        let old_line = std::mem::replace(&mut lines_vec[i], new_line);
+        let (ip, acc) = run_till_infinite_or_end(&lines_vec);
+        if ip == instruction_count {
+            modified_acc = acc;
+            break;
+        }
+        lines_vec[i] = old_line;
+    }
+    println!("acc value when program exists with one modified instruction: {}", modified_acc)
+}
+
+fn run_till_infinite_or_end(instruction_lines: &Vec<String>) -> (usize, i32) {
+    let instruction_count = instruction_lines.len();
     let mut run_instructions: HashSet<usize> = HashSet::new();
     let mut ip = 0;
     let mut acc = 0;
+
     while !run_instructions.contains(&ip) {
         run_instructions.insert(ip);
-        let (op, param) = lines_vec[ip].split_at(4);
+        let (op, param) = instruction_lines[ip].split_at(4);
         match op {
             "jmp " => {
                 let param_num: i32 = match param.parse() {
@@ -40,9 +73,14 @@ fn main() {
                 panic!("Unknown instruction: {} {}!", op, param);
             }
         }
+
+        if ip >= instruction_count {
+            ip = instruction_count;
+            break;
+        }
     }
 
-    println!("acc on first repeated instruction is: {}", acc);
+    (ip, acc)
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
