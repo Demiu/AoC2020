c3b96d8ebb75366fbe0d6224f9eca47d71dd1e93
Demiu <miazgapow@gmail.com>
2020-12-10 18:47:29 +0100

    Day 10 Part 2 solved

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 fn main() {
     let lines = read_lines("./input.txt").expect("Couldn't read input.txt");
     let lines_vec: Vec<_> = lines.collect::<Result<_, _>>().expect("Couldn't collect lines into a vec");
-    let mut numbers: Vec<u64> = lines_vec
+    let mut numbers: Vec<u32> = lines_vec
         .iter()
         .map(|l| l.parse().unwrap())
         .collect();
@@ -14,7 +14,7 @@ fn main() {
     let mut prev_jolt = 0;
     let mut step_ups1 = 0;
     let mut step_ups3 = 1; // the final step up to the device
-    for n in numbers {
+    for n in numbers.iter().copied() {
         match n - prev_jolt {
             1 => step_ups1 += 1,
             2 => (),
@@ -25,6 +25,24 @@ fn main() {
     }
 
     println!("Part 1 answer: {}", step_ups1 * step_ups3);
+
+    let mut prev_jolt = 0;
+    let mut possible_connections: Vec<u64> = vec![0, 0, 1]; // three zeroes to "preseed"
+    for n in numbers {
+        let jolt_gap = n-prev_jolt;
+        for _ in 1..jolt_gap { 
+            // pad the joltages for missing values
+            possible_connections.push(0);
+        }
+        prev_jolt = n;
+
+        let connections_for_n = possible_connections[(n-1) as usize] + 
+                                possible_connections[ n    as usize] + 
+                                possible_connections[(n+1) as usize];
+        possible_connections.push(connections_for_n);
+    }
+
+    println!("Part 2 answer: {}", possible_connections.last().unwrap());
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
