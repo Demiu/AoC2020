cec327ab1d7a48307d2c4fb41639f7265f198105
Demiu <miazgapow@gmail.com>
2020-12-28 04:52:07 +0100

    Day 12 Part 2 solved

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+#[derive(Copy, Clone)]
 enum Action {
     North,
     South,
@@ -41,39 +42,47 @@ impl Direction {
         match action {
             Action::Left => {
                 *self = match (&self,value) {
-                    (Direction::North, 90) => Direction::West,
-                    (Direction::North, 180) => Direction::South,
-                    (Direction::North, 270) => Direction::East,
-                    (Direction::South, 90) => Direction::East,
-                    (Direction::South, 180) => Direction::North,
-                    (Direction::South, 270) => Direction::West,
-                    (Direction::East, 90) => Direction::North,
-                    (Direction::East, 180) => Direction::West,
-                    (Direction::East, 270) => Direction::South,
-                    (Direction::West, 90) => Direction::South,
-                    (Direction::West, 180) => Direction::East,
-                    (Direction::West, 270) => Direction::North,
+                    (Direction::South, 180) | 
+                    (Direction::East, 90) |
+                    (Direction::West, 270) 
+                        => Direction::North,
+                    (Direction::North, 180) | 
+                    (Direction::East, 270) |
+                    (Direction::West, 90) 
+                        => Direction::South,
+                    (Direction::North, 270) | 
+                    (Direction::South, 90) |
+                    (Direction::West, 180) 
+                        => Direction::East,
+                    (Direction::North, 90) | 
+                    (Direction::South, 270) |
+                    (Direction::East, 180) 
+                        => Direction::West,
                     _ => panic!("Unhandled rotation value: {}", value),
                 }
             },
             Action::Right => {
                 *self = match (&self, value) {
-                    (Direction::North, 90) => Direction::East,
-                    (Direction::North, 180) => Direction::South,
-                    (Direction::North, 270) => Direction::West,
-                    (Direction::South, 90) => Direction::West,
-                    (Direction::South, 180) => Direction::North,
-                    (Direction::South, 270) => Direction::East,
-                    (Direction::East, 90) => Direction::South,
-                    (Direction::East, 180) => Direction::West,
-                    (Direction::East, 270) => Direction::North,
-                    (Direction::West, 90) => Direction::North,
-                    (Direction::West, 180) => Direction::East,
-                    (Direction::West, 270) => Direction::South,
+                    (Direction::South, 180) |
+                    (Direction::East, 270) |
+                    (Direction::West, 90) 
+                        => Direction::North,
+                    (Direction::North, 180) |
+                    (Direction::East, 90) |
+                    (Direction::West, 270)
+                        => Direction::South,
+                    (Direction::North, 90) |
+                    (Direction::South, 270) |
+                    (Direction::West, 180) 
+                        => Direction::East,
+                    (Direction::North, 270) |
+                    (Direction::South, 90) |
+                    (Direction::East, 180) 
+                        => Direction::West,
                     _ => panic!("Unhandled rotation value: {}", value),
                 }
             },
-            _ => ()
+            _ => (panic!("Unhandled action in transform_direction"))
         };
     }
 }
@@ -85,6 +94,18 @@ impl Point {
             Direction::South => self.y -= value,
             Direction::East => self.x += value,
             Direction::West => self.x -= value,
+        }
+    }
+
+    fn rotate_around_origin(&mut self, action: Action, value: i32) -> () {
+        match (action, value) {
+            (Action::Right, 90) | (Action::Left, 270) 
+                => *self = Point {x: self.y, y: -self.x},
+            (Action::Right, 270) | (Action::Left, 90) 
+                => *self = Point {x: -self.y, y: self.x},
+            (Action::Right, 180) | (Action::Left, 180) 
+                => *self = Point {x: -self.x, y: -self.y},
+            _ => panic!("Unhandled action in rotate_around_origin"),
         }
     }
 }
@@ -116,14 +137,26 @@ fn main() {
     
     let mut pos = Point{x: 0, y: 0};
     let mut dir = Direction::East;
-    for i in instructions {
-        execute_instruction(i, &mut pos, &mut dir);
+    for i in instructions.iter() {
+        execute_instruction_p1(*i, &mut pos, &mut dir);
     }
 
-    println!("Final position: [{},{}]; Manhattan distance: {}", pos.x, pos.y, pos.x.abs() + pos.y.abs());
+    println!("Final position for part 1: [{},{}]; Manhattan distance: {}", 
+        pos.x, pos.y, pos.x.abs() + pos.y.abs());
+
+    let mut bpos = Point{x: 0, y: 0};
+    let mut wpos = Point{x: 10, y: 1};
+    for i in instructions.iter() {
+        execute_instruction_p2(*i, &mut bpos, &mut wpos);
+    }
+    
+    println!("Final position for part 2: [{},{}]; Manhattan distance: {}", 
+        bpos.x, bpos.y, bpos.x.abs() + bpos.y.abs());
 }
 
-fn execute_instruction(instruction: (Action, i32), position: &mut Point, direction: &mut Direction) -> () {
+fn execute_instruction_p1(instruction: (Action, i32), position: &mut Point, 
+                          direction: &mut Direction) -> () 
+{
     match instruction.0 {
         Action::Left | Action::Right => {
             direction.transform_direction(instruction.0, instruction.1);
@@ -131,11 +164,32 @@ fn execute_instruction(instruction: (Action, i32), position: &mut Point, directi
         Action::North | Action::South | Action::East | Action::West => {
             match instruction.0.to_direction() {
                 Some(d) => position.move_in_direction(&d, instruction.1),
-                None => panic!("Wrong action entered absolute direction move branch"),
+                None => panic!("Wrong action entered direction move branch"),
             }
         },
         Action::Forward => {
             position.move_in_direction(direction, instruction.1);
+        },
+    }
+}
+
+fn execute_instruction_p2(instruction: (Action, i32), boat_position: &mut Point, 
+                          waypoint_position: &mut Point) -> () 
+{
+    match instruction.0 {
+        Action::Left | Action::Right => {
+            waypoint_position.rotate_around_origin(instruction.0, instruction.1);
+        },
+        Action::North | Action::South | Action::East | Action::West => {
+            match instruction.0.to_direction() {
+                Some(d) => waypoint_position.move_in_direction(&d, instruction.1),
+                None => panic!("Wrong action entered waypoint move branch"),
+            }
+        },
+        Action::Forward => {
+            *boat_position = Point{
+                x: boat_position.x + (waypoint_position.x * instruction.1), 
+                y: boat_position.y + (waypoint_position.y * instruction.1)};
         },
     }
 }
