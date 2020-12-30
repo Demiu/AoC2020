38146d92cf15b3375bad813df34fa60e1958bc75
Demiu <miazgapow@gmail.com>
2020-12-30 04:25:52 +0100

    Tidy up Day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,14 +1,11 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::Read;
+use std::fs::{File, read_to_string};
 
 fn main() {
-    let mut file = File::open("./input.txt").expect("Couldn't find input file");
-    let mut file_str = String::new();
-    file.read_to_string(&mut file_str)
+    let file_str = read_to_string("./input.txt")
         .expect("Couldn't read input to string");
 
-    let numbers: Vec<u32> = file_str
+    let mut numbers: Vec<u32> = file_str
         .lines() // the input.txt file has a trailing newline
         .next()
         .expect("Couldn't extract first line from file")
@@ -19,12 +16,12 @@ fn main() {
     
     // last starting number is treated like non-starting
     let mut number_memory = HashMap::new();
-    for i in 0..(numbers.len() - 1) {
-        number_memory.insert(numbers[i], (i+1) as u32);
+    let starting_turn = numbers.len();
+    let mut next_number = numbers.pop().unwrap();
+    for (i, num) in numbers.into_iter().enumerate() {
+        number_memory.insert(num, (i + 1) as u32);
     }
 
-    let starting_turn = numbers.len();
-    let mut next_number = *numbers.last().unwrap();
     for turn in starting_turn..2020 {
         match number_memory.insert(next_number, turn as u32) {
             Some(last_seen) => {
